6d144945ca0f49c6e27b5714e926a761c8b54c54
v-pabst <varyapabst@gmail.com>
2026-03-16 23:54:43 +0500

    Update LR6.cpp

--- LR6/LR6.cpp
+++ LR6/LR6.cpp
@@ -31,3 +31,49 @@ int main()
     cout << kol;
     return 0;
 }
+
+
+using namespace std;
+const int A = 6, B = 10;
+int main()
+{
+	setlocale(LC_ALL, "");
+	srand(time(0));
+	int k;
+	int mas[A][B];
+	int i, j, t;
+	for (i = 0; i < A; i++) {
+		for (j = 0; j < B; j++) {
+			if (j == 0) mas[i][j] = i + 1;
+			else {
+				mas[i][j] = rand()%(100);
+			}
+		}
+	}
+	cout << "Исходный массив:\n";
+	for (i = 0; i < A; i++) {
+		for (j = 0; j < B; j++)
+			printf("%8.2i", mas[i][j]);
+		printf("\n");
+	}
+	cout << "k = ";
+	cin >> k;
+	for (i = 0; i < A; i++) {
+		for (j = B-1; j > i; j--) {
+			if (j == 1)break;
+			if (mas[k-1][j-1] > mas[k-1][j]){
+				for (int row = 0; row < A; row++) {
+					t = mas[row][j - 1];
+					mas[row][j - 1] = mas[row][j];
+					mas[row][j] = t;
+				}
+			}
+		}
+	}
+	cout << "Упорядоченный массив:\n";
+	for (i = 0; i < A; i++) {
+		for (j = 0; j < B; j++)
+			printf("%8.2i", mas[i][j]);
+		printf("\n");
+	}
+}
